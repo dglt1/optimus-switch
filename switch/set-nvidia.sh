56c2ff455ef8667720a2c70e9a9c503392dcb7a4
dglt1 <47616132+dglt1@users.noreply.github.com>
2019-02-14 12:41:44 -0500

    Add files via upload

--- switch/set-nvidia.sh
+++ switch/set-nvidia.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+echo 'Removing intel only mode setup......'
 rm -rf /etc/X11/xorg.conf.d/99-intel.conf
 rm -rf /etc/modprobe.d/99-intel.conf
 rm -rf /etc/modules-load.d/99-intel.conf
 rm -rf /usr/local/bin/optimus.sh
 
 sleep 1
+echo 'Setting nvidia prime mode.......'
 
 cp /etc/switch/nvidia/nvidia-mhwd.conf /etc/X11/mhwd.d/99-nvidia.conf
 cp /etc/switch/nvidia/nvidia-xorg.conf /etc/X11/xorg.conf.d/99-nvidia.conf
@@ -13,4 +15,5 @@ cp /etc/switch/nvidia/nvidia-modprobe.conf /etc/modprobe.d/99-nvidia.conf
 cp /etc/switch/nvidia/nvidia-modules.conf /etc/modules-load.d/99-nvidia.conf
 cp /etc/switch/nvidia/optimus.sh /usr/local/bin/optimus.sh
 
-
+sleep 1
+echo 'Done! After reboot you will be using nvidia prime mode.'
